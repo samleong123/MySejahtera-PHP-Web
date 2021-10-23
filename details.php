d589b1841478aac82cfadfa9b5c4ac7d69162820
Sam Sam <58818070+samleong123@users.noreply.github.com>
2021-10-23 20:34:14 +0800

    Fix typo variable caused 403 even with password & username entered

--- details.php
+++ details.php
@@ -32,7 +32,7 @@ if(empty($mysjid)) {
 <strong>Uh-oh.</br> Seems like you did not provide username to retrieve your MySejahtera details.</br> Please try again later. </br> [403 Forbidden] <strong>'; header("HTTP/1.1 403 Forbidden"); header("Fail_Reason: Did not provide username"); header("backend-status: N/A"); header("frontend-status: 403");
     
 } else {
-    if(empty($mysjpassword)) {echo '<body>
+    if(empty($mysjpword)) {echo '<body>
 <div class="px-4 py-5 my-5 text-center"> <h1 class="display-5 fw-bold">MySejahtera details checker</h1>  </br>
 <h2 class="lead mb-4">Check your MySejahtera details on this website via MySejahtera API.</br>Powered by <a href="https://github.com/samleong123">Sam Sam</a>.</br>Source code : <a href="https://github.com/samleong123/MySejahtera-PHP-Web">here</a>.</h2>
    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
